07daee29a3879ec48e93efec3c427537fac87d88
jinshuilai <haiguang529@foxmail.com>
2017-02-17 16:13:19 +0800

    框架搭建

---
+++ mao-manager/mao-manager-web/src/main/java/com/mao/hotel/controller/admin/AdminController.java
@@ -0,0 +1,15 @@
+package com.mao.hotel.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+	@RequestMapping("/admin")
+	public String admin(){
+		
+		
+		return "index";
+	}
+}
